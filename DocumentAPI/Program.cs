1c58d7a2c8c5910bb48d2f46c1d64911fd6f74bf
David <david.joset@yahoo.fr>
2023-07-25 13:47:10 -0400

    make sure db is created

--- DocumentAPI/Program.cs
+++ DocumentAPI/Program.cs
@@ -1,3 +1,4 @@
+using DocumentAPI.Db;
 using DocumentAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
-builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.ConfigureServices();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // add 10 seconds delay to ensure the db server is up to accept connections
+        // this won't be needed in real world application
+        System.Threading.Thread.Sleep(10000);
+        var context = services.GetRequiredService<FileUserDbContext>();
+        var created = context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
